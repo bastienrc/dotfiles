e25c528be9d6c01f7ad56974c326abe528cc93ff
Sebastien Colbert <colbertsebastien@gmail.com>
2019-12-31 10:58:10 +0100

    Add config Thunar

---
+++ scripts/thunar/thunar/chrome-own.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+    sudo -u chrome chmod go+rwx "$@" -R
+echo "All done. And hit enter key to exit."
+read end;
+
+exit 0
---
+++ scripts/thunar/thunar/file-details.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+    echo 'File details:'
+    ls -haZl "$@"
+echo "All done. And hit enter key to exit."
+read end;
+
+exit 0
